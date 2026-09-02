d331f861a01f6b7e865752c039e2c3f68387da58
SomeGuy143 <apacella@uw.edu>
2026-09-02 14:40:25 -0500

    minimal setup for f# project

---
+++ source/LogParser/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace LogParser
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ source/Run.fsx
@@ -0,0 +1,3 @@
+#r "LogParser/bin/Debug/net10.0/LogParser.dll"
+
+LogParser.Say.hello "World"
